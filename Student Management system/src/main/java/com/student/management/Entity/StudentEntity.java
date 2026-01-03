643e345c784f4d45a02a632627e0172ca86b9f0e
Akash-A2004 <aak957990@gmail.com>
2026-01-03 15:53:22 +0530

    first commit

---
+++ Student Management system/src/main/java/com/student/management/Entity/StudentEntity.java
@@ -0,0 +1,60 @@
+package com.student.management.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "StudentManagement")
+public class StudentEntity {
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String email;
+    private int age;
+    private String course;
+// default constructor
+    public StudentEntity() { }
+// All arguments constructor
+    public StudentEntity(long id, String name, String email, int age, String course) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.course = course;
+    }
+//Getter and setter
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getCourse() {
+        return course;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
